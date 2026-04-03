f9cdc37b140c5a66336288594e4b517e08469c24
Eric Chen <enchen@seas.upenn.edu>
2026-04-02 21:50:32 -0400

    initial xcode project setup

---
+++ Tempo/Tempo/Tempo/TempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoApp.swift
+//  Tempo
+//
+//  Created by Eric Chen on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
